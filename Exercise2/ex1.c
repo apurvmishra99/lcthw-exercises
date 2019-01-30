05392df3b33d4245b23aebfe86590d527298e82d
Apurv <apurvmishra99@gmail.com>
2019-01-30 15:40:16 +0000

    First Commit

---
+++ Exercise2/ex1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/* Long comments are placed like this */
+
+int main(int argc, char *argv[])
+
+{
+	int distance = 100;
+	
+	// short comments
+	
+	printf("You are %d miles away.\n", distance);
+
+	return 0;
+} 
